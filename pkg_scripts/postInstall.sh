dd34b28a38ebe9c4eb50b525afc7671eb0e4152e
Ian McLeod <ianmcleod@carfax.com>
2017-10-18 08:50:34 -0500

    yes

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -3,6 +3,8 @@
 # create log folder
 install  --mode=755 --directory /var/log/spintest
 
+chown ubuntu:ubuntu /opt/spintest/spintest-0.0.1-SNAPSHOT.jar
+
 # start services
 systemctl enable spintest.service
 service spintest start
